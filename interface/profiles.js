d85ae46557f33caab492ebcf3b4520849031d625
Justin <36967116+jcx975@users.noreply.github.com>
2019-04-02 13:02:58 -0500

    Profile test page changes

--- interface/profiles.js
+++ interface/profiles.js
@@ -1,14 +1,47 @@
 
-$(document).ready( function()
+var companyNames = [];
+
+$( document ).ready( function()
 {
 	$( "#profiles-search" ).on( "keyup", profileSearch );
-
+	
+	// Generate random company names for testing purposes.
+	// In the future this won't be done in JavaScript, we'll be reading
+	// from the database server-side.
 	for ( let i = 0; i < 1001; i++ )
 	{
-		$( "#profiles-list" ).append( newCompanyProfile( "Test " + i ) );
+		let length = Math.floor( Math.random() * 10 ) + 1
+
+		companyNames[ i ] = generateName( length );
 	}
+
+	// Sort company names alphabetically
+	companyNames.sort();
+
+	createProfileElements( companyNames );
 } );
 
+// Create a random string of letters of a certain length to simulate
+// company names.
+function generateName( length )
+{
+	let text = "";
+	let letters = "abcdefghijklmnopqrstuvwxyz";
+
+	for ( let i = 0; i < length; i++ )
+	{
+		let random_letter = letters.charAt( Math.floor( Math.random() * letters.length ) );
+
+		if ( i === 0 )
+			random_letter = random_letter.toUpperCase();
+
+		text += random_letter;
+	}
+
+	return text;
+}
+
+// Hides elements that don't match the user's search string.
 function profileSearch()
 {
 	let searchString = $.trim( $( "#profiles-search" ).val().toLowerCase() );
@@ -29,18 +62,24 @@ function profileSearch()
 		$( ".profile-container" ).show();
 }
 
-function newCompanyProfile( company_name )
+// Creates a new company profile page element. This will be done server-
+// side and not in JavaScript.
+function createProfileElements( company_names )
 {
-	let profbox = document.createElement( "div" );
-	let profile = document.createElement( "div" );
+	for ( let i = 0; i < company_names.length; i++ )
+	{
+		let company_name = company_names[ i ];
+		let profbox = document.createElement( "div" );
+		let profile = document.createElement( "div" );
 
-	$( profbox ).addClass( "col-md-6 mb-5 profile-container" );
-	$( profile ).addClass( "company-profile" );
-	$( profile ).append( "<p class='profile-name'>" + company_name + "</p>" );
-	$( profile ).append( "<p>Brief information about company \"" + company_name + ".\"</p>" );
-	$( profile ).append( "<button type='button' class='btn btn-primary'>View Details</button>")
+		$( profbox ).addClass( "col-md-6 mb-5 profile-container" );
+		$( profile ).addClass( "company-profile" );
+		$( profile ).append( "<p class='profile-name'>" + company_name + "</p>" );
+		$( profile ).append( "<p>Brief information about company \"" + company_name + ".\"</p>" );
+		$( profile ).append( "<button type='button' class='btn btn-primary'>View Details</button>")
 
-	$( profbox ).append( profile );
+		$( profbox ).append( profile );
 
-	return profbox;
+		$( "#profiles-list" ).append( profbox );
+	}
 }
