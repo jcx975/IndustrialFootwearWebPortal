1ee91b94d2b45a059f45ff81857eba59c1c970f2
Tim Jensen <timjensen757@gmail.com>
2019-04-09 17:32:51 -0500

    sort company profiles

--- src/main/java/footwearwebportal/CompanyData.java
+++ src/main/java/footwearwebportal/CompanyData.java
@@ -1,7 +1,7 @@
 package footwearwebportal;
 
-public class CompanyData {
-	private String companyID, companyName, city, state;
+public class CompanyData implements Comparable<CompanyData> {
+	public String companyID, companyName, city, state;
 
 	public CompanyData(String companyID, String companyName, String city, String state){
 		this.companyID = companyID;
@@ -13,5 +13,42 @@ public class CompanyData {
 	@Override
 	public String toString() {
 		return companyID + ", " + companyName + ", " + city + ", " + state;
+	}
+
+	public String getCompanyID() {
+		return companyID;
+	}
+
+	public void setCompanyID(String companyID) {
+		this.companyID = companyID;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	@Override
+	public int compareTo(CompanyData s){
+		return this.getCompanyName().toUpperCase().compareTo(s.getCompanyName().toUpperCase());
 	}
 }
--- src/main/java/footwearwebportal/ProfileList.java
+++ src/main/java/footwearwebportal/ProfileList.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.StringWriter;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 
 public class ProfileList extends  HttpServlet {
@@ -13,16 +14,15 @@ public class ProfileList extends  HttpServlet {
 	public String createList() throws IOException, SQLException {
 		StringWriter out = new StringWriter();
 		ArrayList<CompanyData> companyData = data.allCompanyProfiles();
-		Iterator<CompanyData> itr = companyData.iterator();
+		Collections.sort(companyData);
 
-		while(itr.hasNext()){
-			CompanyData company = itr.next();
+		for (CompanyData company : companyData) {
 			out.append("<div class=\"col-md-6 mb-5 profile-container\">");
 			out.append("<div class=\"company-profile\">");
 			out.append("<p class=\"profile-name\">" + company.companyName + "</p>");
 			out.append("<p>" + company.city + ", " + company.state + "</p>");
 			out.append("<form action=\"company.jsp\" method=\"GET\"");
-			out.append("<input type=\"hidden\" name=\"id\" value=\"" + company.id + "\">");
+			out.append("<input type=\"hidden\" name=\"id\" value=\"" + company.companyID + "\">");
 			out.append("<input class=\"btn btn-primary\" type=\"submit\" value=\"View Details\"></form>");
 			out.append("</div>");
 			out.append("</div>");
